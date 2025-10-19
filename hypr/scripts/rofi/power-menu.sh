e37f76c8de87a03a4689356d8329e469932d02bd
Praveen Kumar <mallidipraveenkumarreddy@gmail.com>
2025-10-19 17:04:59 +0530

    (rofi) Improve Power Menu Icons

--- hypr/scripts/rofi/power-menu.sh
+++ hypr/scripts/rofi/power-menu.sh
@@ -3,7 +3,7 @@
 source "$HOME/.config/hypr/scripts/variables.sh"
 
 # Power menu options
-options="  Logout\n  Reboot\n⏻  Shutdown\n  Suspend\n  Lock"
+options="  Logout\n  Reboot\n  Shutdown\n  Suspend\n  Lock"
 
 # Launch Rofi with the options
 chosen=$(echo -e "$options" | rofi -dmenu -p "Power Menu" -config $rofi_default_theme)
@@ -16,7 +16,7 @@ case $chosen in
     "  Reboot")
         systemctl reboot
         ;;
-    "⏻  Shutdown")
+    "  Shutdown")
         systemctl poweroff
         ;;
     "  Suspend")
